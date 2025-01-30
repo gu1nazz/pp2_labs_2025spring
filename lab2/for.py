008226c693c672001d27bb70bee3ef6385177648
Gulnaz <kassymgulnaz007@gmail.com>
2025-01-30 17:00:14 +0500

    lab2

---
+++ lab2/for.py
@@ -0,0 +1,11 @@
+#Print each fruit in a fruit list:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#Loop through the letters in the word "banana":
+
+for x in "banana":
+  print(x)
+
---
+++ lab2/if_else.py
@@ -0,0 +1,13 @@
+#If statement:
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+a = 200
+b = 33
+
+if a > b: print("a is greater than b")
+
+
